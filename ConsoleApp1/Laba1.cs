b15a9a36ff7b3aacad70f42e796ccf29195c77d7
cenya <cenya@511-20>
2026-09-12 14:09:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Laba1.cs
@@ -0,0 +1,22 @@
+﻿////высокий 
+//Console.Write("Введите x: ");
+//double x = double.Parse(Console.ReadLine());
+//double K = (Math.Sqrt(Math.Pow(3+x, 6) - Math.Log
+//    (x))) / (Math.Exp(0)+Math.Asin(6*x*x));
+//Console.WriteLine($"K={K:F2}");
+
+////базовый 
+//Console.WriteLine("Введите y: ");
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите r: ");
+//double r = double.Parse
+
+//Вариант 10 
+
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите t:");
+double t = double.Parse(Console.ReadLine());
+double Z = ((2 * t) + (Math.Cos(y) * t)) / (Math.Exp(0) + 4.831);
+Console.WriteLine($"Z={Z:F2}");
+
---
+++ first1 project/Program.cs
@@ -0,0 +1,22 @@
+﻿////////Console.Write("Введите ваше имя: ");
+////////string name = Console.ReadLine();
+////////Console.WriteLine("Меня зовут " + name);
+
+//////Console.Write("Введите x: ");
+//////int x = int.Parse(Console.ReadLine());
+//////Console.Write("Введите y: ");
+//////int y = Convert.ToInt32(Console.ReadLine());
+//////int s = x + y;
+//////Console.WriteLine($"{x}+{y}={s}");
+
+////Console.Write("угол: ");
+////double angle = double.Parse(Console.ReadLine());
+////double y = Math.Sin(angle * Math.PI / 180);
+////Console.WriteLine($"y={y:F2}");   
+
+//double number = double.Parse(Console.ReadLine());
+//double y = Math.Exp(7 * number);
+//Console.WriteLine($"y={y:f2}");
+
+//Лабораторная работа
+Console.WriteLine();
